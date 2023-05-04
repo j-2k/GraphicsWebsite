026b388f69ec794d56ae2b7e363aad9ea68293da
Juma Al Remeithi <juma_ad@hotmail.com>
2023-05-04 15:12:42 +0400

    pushing logo placement just need to link it to git

--- src/main.ts
+++ src/main.ts
@@ -242,7 +242,7 @@ function CustomControlKeys()
   const exploreText = document.getElementById("clickable-text")as HTMLElement;
   const hiddenText = document.getElementById("hidden-text") as HTMLElement;
   const titleBioText = document.querySelector(".titlebio") as HTMLElement;
-    const aboutStr:string = "Hi there! I'm Juma Al Remeithi, a developer that specializes on Games, Websites, & Graphics. I have been playing games for a very long time (over 10k hours in-game time) & decided to make it my passion. My dream is to work in anything graphics related such as Shaders, Graphics Specializations, Rendering Topics, & etc. This is essentially my biggest dream to work & specialize in the graphics space. Check out my github if you're interested in my on-going projects! github.com/j-2k";
+    const aboutStr:string = "Hi there! I'm Juma Al Remeithi, a developer that specializes on Games, Websites, & Graphics. I have been playing games for a very long time (over 10k hours in-game time) & decided to make it my passion. My dream is to work in anything graphics related such as Shaders, Graphics Specializations, Rendering Topics, & etc. This is essentially my biggest dream to work & specialize in the graphics space. Check out my github by clicking the github logo at the top if you're interested in my on-going projects!";
   
     exploreText.addEventListener("click", function() {
       if(isShowingBio)
