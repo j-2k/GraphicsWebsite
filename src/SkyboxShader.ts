c746ee8a361b1266a49bf1e6581d5c920e6be8a9
Juma Al Remeithi <juma_ad@hotmail.com>
2023-05-04 10:17:19 +0400

    im ditching proc shader might even use a cube texture now not sure

--- src/SkyboxShader.ts
+++ src/SkyboxShader.ts
@@ -1,15 +1,16 @@
 import * as THREE from 'three';
-
+//im dying over doing this procedurally shouldve just used textures.
 const starFunction= `
 float Star(vec2 uv, float flare,float starDown)
 {
     float d = length(uv);
-    float m = .01/d;
+    float m = .05/d;
     
     //float star = max(1. - abs(uv.x * uv.y) * 1000. - 0.2,0.);
     //m += star * flare;
     m -= starDown;
-    return m;
+
+    return max(m,0.);
 }
 `;
 
@@ -44,24 +45,54 @@ const skyboxMaterial = new THREE.ShaderMaterial({
       
       vec2 uv = (vUV * 2. - 1.);
       vec2 uvm = uv;
-      uv *= 10.;
-      vec3 col = vec3(0);
+      vec2 uvc = uv;
+      uvc.x *= 50. * 2.;
+      uvc.y *= 25. * 2.;
+      
+      uvc.y += (uvc.y + time*1.);
+      uvc.x += (uvc.x + time*-1.);
+      
+      
+      /*
+      vec2 uvtl = uv;
+      vec2 uvbr = uv;
+      uvtl *= 10.;
+      uvtl.x -= 10.;
+      uvbr*= 15.;
+      uvbr.x -= 5.;
+      */
+     
+     
+      vec3 col = vec3(0.);
 
-
-      float d = length(uv);
-
-      uv.xy += time*-0.5;
-
-      vec2 gv = fract(uv) - 0.5;
-      col += Star(gv,1.0,0.);
+      vec2 gv = fract(uvc) - 0.5;
+      col += Star(gv,1.0,0.15);
       //col.rg += gv;
 
       float mask = smoothstep(0.6,0.5,abs(uvm.y)) * 1.;
-      float m2 = sin(length(uvm * uvm * 10. + time*2.));
+      float m2 = sin(length(uvm*uvm * 10. + time*2.));
 
-      gl_FragColor = vec4(col * (mask * m2),1);
-      //gl_FragColor = vec4(col,1);
-      //gl_FragColor = vec4(vec3(1) * m2 * mask ,1);
+      
+      /*
+      vec3 b = vec3(.0,.0,.0);
+      vec3 w = vec3(1.0,1.0,1.0);
+      uvc *= 10.;
+      float c = float(mix(b, w , abs(uvc.x+ sin(fract(time*1.1) * 3. - 1.5) * 90.)));
+      float c2 = float(mix(b, w , abs(uvc.y+ sin(fract(time*1.1) * 3. - 1.5) * 90.)));
+      float ctl = float(mix(b, w , abs(uvtl.x+ sin(fract(time*1.) * 3. - 1.5) * 10.)));
+      float ctl2 = float(mix(b, w , abs(uvtl.y+ sin(fract(time*1.) * 3. - 1.5) * 10.)));
+      float cbr =  float(mix(b, w , abs(uvbr.x+ sin(fract(time*1.) * 3. - 1.5) * 25.)));
+      float cbr2 = float(mix(b, w , abs(uvbr.y+ sin(fract(time*1.) * 3. - 1.5) * 25.)));
+      float fc = 1.-((c+c2)-10.);
+      float fc2 = 1.-((ctl+ctl2)- 1.);
+      float fc3 = 1.-((cbr+cbr2)-1.);
+      
+      float fCols = max((fc * fc2 * fc3),0.);
+      col *= mask;
+      col *= fCols;
+      */
+      //gl_FragColor = vec4(vec3(col),1.);
+      gl_FragColor = vec4(vec3(col) * (m2 * mask),1.);
     }
   `,
   uniforms: {
