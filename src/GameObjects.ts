49e779c1c4c32100e5f25f6b9249d440cb64a982
Juma Al Remeithi <juma_ad@hotmail.com>
2023-05-15 08:31:34 +0400

    GameObjects file added removed the huge clump of adding objects in the main file.

---
+++ src/GameObjects.ts
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+const GameObjects: THREE.Mesh[] = [];
+const DebugGameObjects: THREE.LineSegments[] = [];
+
+// Create a sphere
+const geometry = new THREE.SphereGeometry(1,16,16);
+const cubeGeometry = new THREE.BoxGeometry(50,9,200);
+const material = new THREE.MeshPhongMaterial({ color: "#ff00ff", shininess: 200, specular: 0x444444});
+const sphereMesh = new THREE.Mesh(geometry, material);
+const material2 = new THREE.MeshPhongMaterial({ color: "#00ff55"});
+
+const sphereMeshL = new THREE.Mesh(geometry, material2);
+const sphereMeshR = new THREE.Mesh(geometry, material);
+const cubeMesh = new THREE.Mesh(cubeGeometry, material2);
+sphereMeshL.position.y = -5;
+sphereMeshL.position.z = -1;
+sphereMeshR.position.x = -10;
+sphereMesh.position.y = 5;
+cubeMesh.position.y = -20;
+GameObjects.push(sphereMesh);
+GameObjects.push(sphereMeshL);
+GameObjects.push(sphereMeshR);
+GameObjects.push(cubeMesh);
+const cg2 = new THREE.BoxGeometry(30,30,30);
+const c_dtestm = new THREE.Mesh(cg2, material2);
+c_dtestm.position.y  = -20;
+c_dtestm.position.z  = -20;
+c_dtestm.position.x  = -20;
+GameObjects.push(c_dtestm);
+
+//Wireframes
+const wireframe = new THREE.WireframeGeometry( geometry );
+const line = new THREE.LineSegments( wireframe );
+line.material.depthTest = true;
+line.material.opacity = 0.25;
+line.material.opacity = true;
+
+DebugGameObjects.push(line);
+
+function test()
+{
+    console.log(GameObjects + " Hello");
+    console.log(DebugGameObjects);
+}
+
+const Mats = {
+    material,
+    line,
+    sphereMesh
+}
+
+//i thought this would be a good idea to just export 1 thing & reference from it,
+//but now I think its just better to export individual stuff... not sure yet,
+//new to typescript & its "good practises", i guess it would depend on the # of things im exporting...
+const AllGameObjects = {
+    GameObjects,
+    DebugGameObjects,
+    test,
+    Mats
+}
+
+
+
+export {
+    AllGameObjects
+}
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import 'three/src/math/MathUtils.js';
 import { depthRenderTarget, postScene, postCamera, depthMaterial } from './depthTarget.ts';
 import {AboutButtonHandler} from './TextHandling.ts';
 AboutButtonHandler();
+import { AllGameObjects } from './GameObjects.ts';
+AllGameObjects.test();
 //import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 //import { gsap } from "gsap";
 
@@ -54,55 +56,15 @@ renderer.setSize(sizes.width,sizes.height);
 // Add the renderer to the document
 document.body.appendChild(renderer.domElement);
 
-// Create a sphere
-const geometry = new THREE.SphereGeometry(1,16,16);
-const cubeGeometry = new THREE.BoxGeometry(50,9,200);
-const material = new THREE.MeshPhongMaterial({ color: "#ff00ff", shininess: 200, specular: 0x444444});
-const sphereMesh = new THREE.Mesh(geometry, material);
-const material2 = new THREE.MeshPhongMaterial({ color: "#00ff55"});
 
-const sphereMeshL = new THREE.Mesh(geometry, material2);
-const sphereMeshR = new THREE.Mesh(geometry, material);
-const cubeMesh = new THREE.Mesh(cubeGeometry, material2);
-sphereMeshL.position.y = -5;
-sphereMeshL.position.z = -1;
-sphereMeshR.position.x = -10;
-sphereMesh.position.y = 5;
-cubeMesh.position.y = -20;
-scene.add(sphereMesh);
-scene.add(sphereMeshL,sphereMeshR,cubeMesh);
-const cg2 = new THREE.BoxGeometry(30,30,30);
-const c_dtestm = new THREE.Mesh(cg2, material2);
-c_dtestm.position.y  = -20;
-c_dtestm.position.z  = -20;
-c_dtestm.position.x  = -20;
-scene.add(c_dtestm);
 
 //Light
 const light = new THREE.PointLight(0xffffff,1,100);
 light.position.set(10,10,10);
 scene.add(light);
 
-
-//Wireframes
-const wireframe = new THREE.WireframeGeometry( geometry );
-const line = new THREE.LineSegments( wireframe );
-line.material.depthTest = true;
-line.material.opacity = 0.25;
-line.material.opacity = true;
-
-scene.add(line);
 scene.add(camera);
 
-
-/*
-const loop = () => {
-  sphereMesh.position.x += 0.01;
-  renderer.render(scene,camera)
-  window.requestAnimationFrame(loop)
-}
-loop()
-*/
 const clock = new THREE.Clock();
 const uniformData = {
   time:{
@@ -114,6 +76,14 @@ const uniformData = {
 //ADDING ALL MY SHADERS HERE
 scene.add(CustomWaterPlane());
 scene.add(CustomSkyboxMesh());
+
+//Adding Game Objects
+AllGameObjects.GameObjects.forEach((gameObject)=>{
+  scene.add(gameObject);
+})
+AllGameObjects.DebugGameObjects.forEach((gameObject)=>{
+  scene.add(gameObject);
+})
 
 //Controls
 //const controls = new OrbitControls(camera, renderer.domElement);
@@ -136,13 +106,6 @@ function Update() {
   requestAnimationFrame(Update);
   CustomControlKeys(camera,deltaTime);
   TimeVarForShaders();
-  material.color.setRGB(
-    THREE.MathUtils.lerp(0,1,sinTime01(2,0.5,1.5)),
-    THREE.MathUtils.lerp(0,1,sinTime01(1.5,0.5,-1.5)),
-    THREE.MathUtils.lerp(0,1,sinTime01(1,0.5,0))
-    );
-  sphereMesh.scale.setScalar(sinTime01(1,0.5,0) * 1);
-
 
   if(clock.elapsedTime > 0.5 + a)
   {
@@ -150,15 +113,21 @@ function Update() {
     a = clock.elapsedTime;
   }
   
-
+  AllGameObjects.Mats.material.color.setRGB(
+    THREE.MathUtils.lerp(0,1,sinTime01(2,0.5,1.5)),
+    THREE.MathUtils.lerp(0,1,sinTime01(1.5,0.5,-1.5)),
+    THREE.MathUtils.lerp(0,1,sinTime01(1,0.5,0))
+    );
+    AllGameObjects.Mats.sphereMesh.scale.setScalar(sinTime01(1,0.5,0) * 1);
 
   // Rotate the sphere with the wireframe
-  sphereMesh.rotation.x += 0.01;
-  sphereMesh.rotation.y += 0.05;
-  sphereMesh.rotation.z += 0.02;
-  line.rotation.x += 0.01;
-  line.rotation.y += 0.05;
-  line.rotation.z += 0.02;
+  AllGameObjects.Mats.sphereMesh.rotation.x += 0.01;
+  AllGameObjects.Mats.sphereMesh.rotation.y += 0.05;
+  AllGameObjects.Mats.sphereMesh.rotation.z += 0.02;
+  AllGameObjects.Mats.line.rotation.x += 0.01;
+  AllGameObjects.Mats.line.rotation.y += 0.05;
+  AllGameObjects.Mats.line.rotation.z += 0.02;
+  
 
   renderer.setRenderTarget( depthRenderTarget );
   renderer.render( scene, camera );
@@ -181,14 +150,4 @@ function TimeVarForShaders()
   uniformData.time.value = clock.getElapsedTime();
   skyboxMaterial.uniforms.time.value = uniformData.time.value
   WaterShaderMaterial.uniforms.u_time.value = uniformData.time.value
-}
-
-
-
-
-//renderer.render(scene,camera);
-export {
-  scene,
-  uniformData,
-  camera
 }
